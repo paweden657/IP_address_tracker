3c7acfde29636fd3ec52adfb6f398f007f1f6689
Paweł <pawelo247@icloud.com>
2023-05-15 20:02:17 +0200

    Add Ip validation system.

--- index.js
+++ index.js
@@ -3,14 +3,22 @@ const url = "http://localhost:5050/";
 const form = document.getElementById("form");
 const input = document.getElementById("input")
 
+let notValidIp = document.getElementById("not-valid-ip");
+
 form.addEventListener("submit", function(event) {
   event.preventDefault();
   const IpAddress = input.value;
+
 
   fetch(`${url}?param=${encodeURIComponent(IpAddress)}`)
     .then(response => response.json())
     .then(data => {
         console.log(data);
+        if(data.Answer === "Address is valid") {
+          notValidIp.classList.add("hide");
+        } else {
+          notValidIp.classList.remove("hide");
+        }
     })
     .catch(error => {
       console.error('error occured', error);
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
-const axios = require("axios");
 const cors = require("cors");
+
+const ip = require("ip-address");
+const axios = require("axios");
 
 require("dotenv").config();
 
@@ -11,21 +13,28 @@ app.use(cors());
 app.get('/', (req, res) => {
     try {
         let IpAddress = req.query.param;
-        console.log(IpAddress);
-        //const apiKey = process.env.API_KEY;
+        if(ip.Address6.isValid(IpAddress) || ip.Address4.isValid(IpAddress)) {
+          console.log("Ip address is valid");
+          //const apiKey = process.env.API_KEY;
   
-        //const response = await axios.get("https://geo.ipify.org/api/v2/country,city?apiKey=" + apiKey + "&ipAddress=" + IpAddress);
+          //const response = await axios.get("https://geo.ipify.org/api/v2/country,city?apiKey=" + apiKey + "&ipAddress=" + IpAddress);
 
-        //const data = response.data;
+          //const data = response.data;
 
-        res.send('{"Otrzymano_zapytanie":"hello!"}');
+          console.log(IpAddress);
+          res.send('{"Answer":"Address is valid"}');
+        } else {
+          console.log("Ip address is not valid");
+          console.log(IpAddress);
+          res.send('{"Answer":"Adres is not valid"}');
+        }
 
     } catch (error) {
       console.error(error);
       res.status(500).send('error occured');
     }
   });
-  
+
   app.listen(5050, () => {
     console.log('Server working on port: 5050');
   });
